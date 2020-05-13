070bd50c623d8a9415186fd8ee86eae0fcadfa53
stdstring <std_string@mail.ru>
2020-05-13 11:36:51 +0500

    use two pointer approach for task "1. Two Sum"

--- Algorithms/0001.TwoSum/solution.h
+++ Algorithms/0001.TwoSum/solution.h
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> indicesMap;
+    std::vector<int> twoSum(std::vector<int> const &nums, int target) {
+        // create and sort vector of indices
+        std::vector<int> indices;
+        indices.reserve(nums.size());
         for (int index = 0; index < nums.size(); ++index)
         {
-            std::unordered_map<int, int>::const_iterator it = indicesMap.find(target - nums[index]);
-            if (it != indicesMap.cend())
-                return {it->second, index};
-            if (indicesMap.find(nums[index]) == indicesMap.cend())
-                indicesMap.emplace(nums[index], index);
+            indices.push_back(index);
+        }
+        std::sort(indices.begin(), indices.end(), [&nums](int left, int right){ return nums[left] < nums[right]; });
+        // use two pointer approach
+        int left = 0;
+        int right = indices.size() - 1;
+        while (left != right)
+        {
+            int value = nums[indices[left]] + nums[indices[right]];
+            if (value == target)
+            {
+                return {indices[left], indices[right]};
+            }
+            else if (value < target)
+            {
+                ++left;
+            }
+            else if (value > target)
+            {
+                --right;
+            }
         }
         return {};
     }
