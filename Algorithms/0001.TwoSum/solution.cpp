8d37d0f95b2bda45a9e97e66c950727d27cae49e
stdstring <std_string@mail.ru>
2020-06-12 13:22:35 +0500

    started converting to utests

--- Algorithms/0001.TwoSum/solution.cpp
+++ Algorithms/0001.TwoSum/solution.cpp
@@ -1,7 +1,15 @@
+#include <algorithm>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
 class Solution
 {
 public:
-    std::vector<int> twoSum(std::vector<int> const &nums, int target)
+    std::vector<int> twoSum(std::vector<int> const &nums, int target) const
     {
         // create and sort vector of indices
         std::vector<int> indices;
@@ -16,20 +24,33 @@ public:
         int right = indices.size() - 1;
         while (left != right)
         {
-            int value = nums[indices[left]] + nums[indices[right]];
+            const int value = nums[indices[left]] + nums[indices[right]];
             if (value == target)
-            {
                 return {indices[left], indices[right]};
-            }
-            else if (value < target)
-            {
+            if (value < target)
                 ++left;
-            }
             else if (value > target)
-            {
                 --right;
-            }
         }
         return {};
     }
 };
+
+}
+
+namespace TwoSumTask
+{
+
+TEST(TwoSumTaskTests, Examples)
+{
+    const Solution solution;
+    ASSERT_EQ(std::vector<int>({0, 1}), solution.twoSum({2, 7, 11, 15}, 9));
+}
+
+TEST(TwoSumTaskTests, FromWrongAnswers)
+{
+    const Solution solution;
+    ASSERT_EQ(std::vector<int>({1, 2}), solution.twoSum({3, 2, 4}, 6));
+}
+
+}
