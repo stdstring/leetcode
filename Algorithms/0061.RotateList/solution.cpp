3738df9b65b54cd5bc375923f2d622fd27cf0cad
stdstring <std_string@mail.ru>
2020-06-30 14:42:29 +0500

    added task "61. Rotate List"

---
+++ Algorithms/0061.RotateList/solution.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+
+#include "ListNode.h"
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+using CommonDefs::ListNode;
+
+class Solution
+{
+public:
+    ListNode* rotateRight(ListNode* head, int k) const
+    {
+        if (head == nullptr)
+            return head;
+        std::vector<ListNode*> nodes;
+        while (head != nullptr)
+        {
+            nodes.push_back(head);
+            head = head->next;
+        }
+        const size_t shift = static_cast<size_t>(k) % nodes.size();
+        if (shift == 0)
+            return nodes[0];
+        head = nodes[nodes.size() - shift];
+        ListNode* current = head;
+        for (size_t index = 1; index < nodes.size(); ++index)
+        {
+            current->next = nodes[(nodes.size() - shift + index) % nodes.size()];
+            current = current->next;
+        }
+        current->next = nullptr;
+        return head;
+    }
+};
+
+}
+
+using CommonDefs::createLinkedList;
+using CommonDefs::checkAndDeleteLinkedList;
+
+namespace RotateListTask
+{
+
+TEST(RotateListTaskTests, Examples)
+{
+    const Solution solution;
+    checkAndDeleteLinkedList({4, 5, 1, 2, 3}, solution.rotateRight(createLinkedList({1, 2, 3, 4, 5}, false).get(), 2));
+    checkAndDeleteLinkedList({2, 0, 1}, solution.rotateRight(createLinkedList({0, 1, 2}, false).get(), 4));
+}
+
+TEST(RotateListTaskTests, FromWrongAnswers)
+{
+    const Solution solution;
+    checkAndDeleteLinkedList({}, solution.rotateRight(createLinkedList({}, false).get(), 0));
+}
+
+}
--- README.md
+++ README.md
@@ -34,6 +34,7 @@ My solutions for tasks from [LeetCode](https://leetcode.com/problemset/all/):
 * [54. Spiral Matrix](https://leetcode.com/problems/spiral-matrix/description/)
 * [56. Merge Intervals](https://leetcode.com/problems/merge-intervals/description/)
 * [58. Length of Last Word](https://leetcode.com/problems/length-of-last-word/description/)
+* [61. Rotate List](https://leetcode.com/problems/rotate-list/description/)
 * [65. Valid Number](https://leetcode.com/problems/valid-number/description/)
 * [66. Plus One](https://leetcode.com/problems/plus-one/description/)
 * [67. Add Binary](https://leetcode.com/problems/add-binary/description/)
