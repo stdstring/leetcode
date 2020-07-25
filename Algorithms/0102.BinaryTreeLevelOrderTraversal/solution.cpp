d83829213bc9eb4b52089a3fcda97913c8574463
stdstring <std_string@mail.ru>
2020-07-25 09:57:51 +0500

    added task "102. Binary Tree Level Order Traversal"

---
+++ Algorithms/0102.BinaryTreeLevelOrderTraversal/solution.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+
+#include "TreeNode.h"
+
+#include "gtest/gtest.h"
+
+using CommonDefs::TreeNode;
+
+namespace
+{
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode* root) const
+    {
+        std::vector<std::vector<int>> dest;
+        levelOrder(root, 0, dest);
+        return dest;
+    }
+
+private:
+    void levelOrder(TreeNode* root, size_t level, std::vector<std::vector<int>> &dest) const
+    {
+        if (root == nullptr)
+            return;
+        if (level == dest.size())
+            dest.emplace_back();
+        dest[level].push_back(root->val);
+        levelOrder(root->left, level + 1, dest);
+        levelOrder(root->right, level + 1, dest);
+    }
+};
+
+}
+
+using CommonDefs::createTreeHolder;
+
+namespace BinaryTreeLevelOrderTraversalTask
+{
+
+TEST(BinaryTreeLevelOrderTraversalTaskTests, Examples)
+{
+    const Solution solution;
+    ASSERT_EQ(std::vector<std::vector<int>>({{3}, {9, 20}, {15, 7}}), solution.levelOrder(createTreeHolder(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))).get()));
+}
+
+}
