8a305219b1966006450d9c62c2720416e1ebd782
stdstring <std_string@mail.ru>
2020-08-18 12:32:21 +0500

    added task "47. Permutations II"

---
+++ Algorithms/0047.PermutationsII/solution.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> permuteUnique(std::vector<int> const &nums) const
+    {
+        std::vector<int> numbers(nums);
+        std::sort(numbers.begin(), numbers.end());
+        std::vector<bool> usedNumbers;
+        for (size_t index = 0; index < numbers.size(); ++index)
+            usedNumbers.push_back(false);
+        std::vector<int> current;
+        std::vector<std::vector<int>> dest;
+        generate(numbers, usedNumbers, current, dest);
+        return dest;
+    }
+
+private:
+    void generate(std::vector<int> const &numbers, std::vector<bool> &usedNumbers, std::vector<int> &current, std::vector<std::vector<int>> &dest) const
+    {
+        int lastUsedIndex = -1;
+        for (size_t index = 0; index < numbers.size(); ++index)
+        {
+            if (!usedNumbers[index])
+            {
+                if (lastUsedIndex != -1 && numbers[lastUsedIndex] == numbers[index])
+                    continue;
+                lastUsedIndex = index;
+                current.push_back(numbers[index]);
+                const bool isPermutation = current.size() == numbers.size();
+                if (isPermutation)
+                    dest.push_back(current);
+                else
+                {
+                    usedNumbers[index] = true;
+                    generate(numbers, usedNumbers, current, dest);
+                    usedNumbers[index] = false;
+                }
+                current.pop_back();
+                if (isPermutation)
+                    return;
+            }
+        }
+    }
+};
+
+}
+
+namespace PermutationsIITask
+{
+
+TEST(PermutationsIITaskTests, Examples)
+{
+    const Solution solution;
+    ASSERT_EQ(std::vector<std::vector<int>>({{1, 1, 2}, {1, 2, 1}, {2, 1, 1}}), solution.permuteUnique({1, 1, 2}));
+}
+
+}
--- README.md
+++ README.md
@@ -31,6 +31,7 @@ My solutions for tasks from [LeetCode](https://leetcode.com/problemset/all/):
 * [41. First Missing Positive](https://leetcode.com/problems/first-missing-positive/description/)
 * [43. Multiply Strings](https://leetcode.com/problems/multiply-strings/description/)
 * [46. Permutations](https://leetcode.com/problems/permutations/description/)
+* [47. Permutations II](https://leetcode.com/problems/permutations-ii/description/)
 * [48. Rotate Image](https://leetcode.com/problems/rotate-image/description/)
 * [49. Group Anagrams](https://leetcode.com/problems/group-anagrams/description/)
 * [50. Pow(x, n)](https://leetcode.com/problems/powx-n/description/)
