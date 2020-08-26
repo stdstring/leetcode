6877ca98e832478b648844a9b478ebdf18b02539
stdstring <std_string@mail.ru>
2020-08-26 14:37:58 +0500

    improved task "13. Roman to Integer"

--- Algorithms/0013.RomanToInteger/solution.cpp
+++ Algorithms/0013.RomanToInteger/solution.cpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <unordered_map>
 
 #include "gtest/gtest.h"
 
@@ -9,32 +8,42 @@ namespace
 class Solution
 {
 public:
-    int romanToInt(std::string const &s)
+    int romanToInt(std::string const &s) const
     {
         int result = 0;
         for (size_t index = 0; index < s.size(); ++index)
-        {
-            result += (isSubtraction(s, index) ? -mRomanSymbols[s[index]] : mRomanSymbols[s[index]]);
-        }
+            result += (isSubtraction(s, index) ? -getRomanSymbolValue(s[index]) : getRomanSymbolValue(s[index]));
         return result;
     }
 
 private:
-    bool isSubtraction(std::string const &s, size_t index)
+    bool isSubtraction(std::string const &s, size_t index) const
     {
-        return index < (s.size() - 1) && mRomanSymbols[s[index]] < mRomanSymbols[s[index + 1]];
+        return index < (s.size() - 1) && getRomanSymbolValue(s[index]) < getRomanSymbolValue(s[index + 1]);
     }
 
-    std::unordered_map<char, int> mRomanSymbols =
+    int getRomanSymbolValue(char romanSymbol) const
     {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
+        switch (romanSymbol)
+        {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
+        }
+    }
 };
 
 }
@@ -44,7 +53,7 @@ namespace RomanToIntegerTask
 
 TEST(RomanToIntegerTaskTests, Examples)
 {
-    Solution solution;
+    const Solution solution;
     ASSERT_EQ(3, solution.romanToInt("III"));
     ASSERT_EQ(4, solution.romanToInt("IV"));
     ASSERT_EQ(9, solution.romanToInt("IX"));
