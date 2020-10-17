c6bd527e2129b89689a70308a62d5f6d7f475b38
stdstring <std_string@mail.ru>
2020-10-17 12:45:45 +0500

    added task "162. Find Peak Element"

---
+++ Algorithms/0162.FindPeakElement/solution.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+class Solution
+{
+public:
+    int findPeakElement(std::vector<int> const &nums) const
+    {
+        if (nums.size() == 1)
+            return 0;
+        if (nums[0] > nums[1])
+            return 0;
+        for (size_t index = 1; index < nums.size() - 1; ++index)
+        {
+            if ((nums[index - 1] < nums[index]) && (nums[index + 1] < nums[index]))
+                return index;
+        }
+        return static_cast<int>(nums.size() - 1);
+    }
+};
+
+}
+
+namespace FindPeakElementTask
+{
+
+TEST(FindPeakElementTaskTests, Examples)
+{
+    const Solution solution;
+    ASSERT_EQ(2, solution.findPeakElement({1, 2, 3, 1}));
+    ASSERT_EQ(1, solution.findPeakElement({1, 2, 1, 3, 5, 6, 4}));
+}
+
+TEST(FindPeakElementTaskTests, FromWrongAnswers)
+{
+    const Solution solution;
+    ASSERT_EQ(0, solution.findPeakElement({1}));
+    ASSERT_EQ(1, solution.findPeakElement({1, 2}));
+}
+
+}
