fca1469aa78ce8c9a5436bc5460a0c572d53b982
stdstring <std_string@mail.ru>
2020-12-11 19:52:37 +0500

    added task "105. Construct Binary Tree from Preorder and Inorder Traversal"

---
+++ Algorithms/0105.ConstructBinaryTreeFromPreorderInorderTraversal/solution.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+
+#include "TreeNode.h"
+#include "TreeNodeUtils.h"
+
+#include "gtest/gtest.h"
+
+using CommonLib::TreeNode;
+
+namespace
+{
+
+class Solution
+{
+public:
+    TreeNode* buildTree(std::vector<int> const &preorder, std::vector<int> const &inorder) const
+    {
+        size_t preorderIndex = 0;
+        return buildTree(preorder, inorder, preorderIndex, 0, inorder.size());
+    }
+
+private:
+    TreeNode* buildTree(std::vector<int> const &preorder, std::vector<int> const &inorder, size_t &preorderIndex, size_t inorderStart, size_t inorderRangeSize) const
+    {
+        if (inorderRangeSize == 0)
+            return nullptr;
+        const int currentValue = preorder[preorderIndex++];
+        if (inorderRangeSize == 1)
+            return new TreeNode(currentValue);
+        const size_t inorderValueIndex = findValueInorderIndex(inorder, inorderStart, inorderRangeSize, currentValue);
+        const size_t leftSize = inorderValueIndex - inorderStart;
+        TreeNode* leftSubtree = buildTree(preorder, inorder, preorderIndex, inorderStart, leftSize);
+        const size_t rightSize = inorderRangeSize - leftSize - 1;
+        TreeNode* rightSubtree = buildTree(preorder, inorder, preorderIndex, inorderValueIndex + 1, rightSize);
+        return new TreeNode(currentValue, leftSubtree, rightSubtree);
+    }
+
+    size_t findValueInorderIndex(std::vector<int> const &inorder, size_t inorderStart, size_t inorderRangeSize, int value) const
+    {
+        for (size_t shift = 0; shift < inorderRangeSize; ++shift)
+        {
+            if (inorder[inorderStart + shift] == value)
+                return inorderStart + shift;
+        }
+        throw std::out_of_range("value");
+    }
+};
+
+}
+
+using CommonLib::Codec;
+using CommonLib::createTreeHolder;
+
+namespace ConstructBinaryTreeFromPreorderInorderTraversalTask
+{
+
+TEST(ConstructBinaryTreeFromPreorderInorderTraversalTaskTests, Examples)
+{
+    const Solution solution;
+    ASSERT_EQ("[3,9,20,null,null,15,7]", Codec::createData(createTreeHolder(solution.buildTree({3, 9, 20, 15, 7}, {9, 3, 15, 20, 7}))));
+}
+
+}
--- README.md
+++ README.md
@@ -87,6 +87,7 @@ My solutions for tasks from [LeetCode](https://leetcode.com/problemset/all/):
 * [102. Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/description/)
 * [103. Binary Tree Zigzag Level Order Traversal](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/)
 * [104. Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree/description/)
+* [105. Construct Binary Tree from Preorder and Inorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/)
 * [107. Binary Tree Level Order Traversal II](https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/)
 * [108. Convert Sorted Array to Binary Search Tree](https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/)
 * [109. Convert Sorted List to Binary Search Tree](https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/)
