a0236df15a6a53bed36ab28a7969e97263efb596
stdstring <std_string@mail.ru>
2021-05-23 22:01:02 +0500

    improved task "74. Search a 2D Matrix"

--- Algorithms/0074.Search2DMatrix/solution.cpp
+++ Algorithms/0074.Search2DMatrix/solution.cpp
@@ -1,4 +1,3 @@
-#include <tuple>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -11,69 +10,27 @@ class Solution
 public:
     bool searchMatrix(std::vector<std::vector<int>> const &matrix, int target) const
     {
-        if (matrix.empty())
+        if ((target < matrix.front().front()) || (target > matrix.back().back()))
             return false;
-        if (matrix.front().empty())
-            return false;
-        if (matrix.front().front() > target)
-            return false;
-        if (matrix.back().back() < target)
-            return false;
-        // search row
-        bool searchRowResult;
-        size_t rowIndex;
-        std::tie(searchRowResult, rowIndex) = searchRow(matrix, target);
-        if (!searchRowResult)
-            return false;
-        // search value
-        return searchValue(matrix[rowIndex], target);
-    }
-
-private:
-    std::tuple<bool, size_t> searchRow(std::vector<std::vector<int>> const &matrix, int target) const
-    {
-        if (matrix.front().front() <= target && target <= matrix.front().back())
-            return std::make_tuple(true, 0);
-        if (matrix.back().front() <= target && target <= matrix.back().back())
-            return std::make_tuple(true, matrix.size() - 1);
+        if ((target == matrix.front().front()) || (target == matrix.back().back()))
+            return true;
+        const size_t rowCount = matrix.size();
+        const size_t columnCount = matrix.front().size();
+        const size_t cellCount = rowCount * columnCount;
         size_t left = 0;
-        size_t right = matrix.size() - 1;
-        while ((right - left) > 1)
+        size_t right = cellCount - 1;
+        while (left <= right)
         {
             const size_t middle = (left + right) / 2;
-            if (matrix[middle].front() <= target && target <= matrix[middle].back())
-                return std::make_tuple(true, middle);
-            if (target < matrix[middle].front())
-                right = middle;
-            else
-                left = middle;
-        }
-        if (matrix[left].front() <= target && target <= matrix[left].back())
-            return std::make_tuple(true, left);
-        if (matrix[right].front() <= target && target <= matrix[right].back())
-            return std::make_tuple(true, right);
-        return std::make_tuple(false, 0);
-    }
-
-    bool searchValue(std::vector<int> const &row, int target) const
-    {
-        if (row.front() == target)
-            return true;
-        if (row.back() == target)
-            return true;
-        size_t left = 0;
-        size_t right = row.size() - 1;
-        while ((right - left) > 1)
-        {
-            const size_t middle = (left + right) / 2;
-            if (row[middle] == target)
+            const int value = matrix[middle / columnCount][middle % columnCount];
+            if (target == value)
                 return true;
-            if (target < row[middle])
-                right = middle;
+            if (target > value)
+                left = middle + 1;
             else
-                left = middle;
+                right = middle - 1;
         }
-        return row[left] == target || row[right] == target;
+        return false;
     }
 };
 
@@ -92,8 +49,8 @@ TEST(Search2DMatrixTaskTests, Examples)
 TEST(Search2DMatrixTaskTests, FromWrongAnswers)
 {
     const Solution solution;
-    ASSERT_EQ(false, solution.searchMatrix({}, 0));
-    ASSERT_EQ(false, solution.searchMatrix({{}}, 1));
+    ASSERT_EQ(false, solution.searchMatrix({{1}}, 0));
+    ASSERT_EQ(true, solution.searchMatrix({{1}}, 1));
 }
 
 }
