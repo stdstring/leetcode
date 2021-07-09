24112979d6c1816b3ec646e9f874db673514774a
stdstring <std_string@mail.ru>
2021-07-09 11:43:19 +0500

    added solution via priority queue into task "215. Kth Largest Element in an Array"

--- Algorithms/0215.KthLargestElementInArray/solution.cpp
+++ Algorithms/0215.KthLargestElementInArray/solution.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <queue>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -9,11 +10,25 @@ namespace
 class Solution
 {
 public:
-    int findKthLargest(std::vector<int> const &nums, int k) const
+    int findKthLargestViaSorting(std::vector<int> const &nums, int k) const
     {
         std::vector<int> result(nums);
         std::sort(result.begin(), result.end());
         return result[nums.size() - k];
+    }
+
+    int findKthLargestViaPriorityQueue(std::vector<int> const& nums, int k) const
+    {
+        const size_t queueSize = k;
+        std::priority_queue<int, std::vector<int>, std::greater<>> numbersQueue;
+        for (int number : nums)
+        {
+            if (numbersQueue.size() < queueSize || number > numbersQueue.top())
+                numbersQueue.push(number);
+            if (numbersQueue.size() > queueSize)
+                numbersQueue.pop();
+        }
+        return numbersQueue.top();
     }
 };
 
@@ -22,11 +37,18 @@ public:
 namespace KthLargestElementInArrayTask
 {
 
-TEST(KthLargestElementInArrayTaskTests, Examples)
+TEST(KthLargestElementInArrayTaskTests, ExamplesViaSorting)
 {
     const Solution solution;
-    ASSERT_EQ(5, solution.findKthLargest({3, 2, 1, 5, 6, 4}, 2));
-    ASSERT_EQ(4, solution.findKthLargest({3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+    ASSERT_EQ(5, solution.findKthLargestViaSorting({3, 2, 1, 5, 6, 4}, 2));
+    ASSERT_EQ(4, solution.findKthLargestViaSorting({3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+}
+
+TEST(KthLargestElementInArrayTaskTests, ExamplesViaPriorityQueue)
+{
+    const Solution solution;
+    ASSERT_EQ(5, solution.findKthLargestViaPriorityQueue({ 3, 2, 1, 5, 6, 4 }, 2));
+    ASSERT_EQ(4, solution.findKthLargestViaPriorityQueue({ 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4));
 }
 
 }
