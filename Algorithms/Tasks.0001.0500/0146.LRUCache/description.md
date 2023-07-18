4688d06945d788e5b2a1bbb13da1529fbc4d881f
stdstring <std_string@mail.ru>
2023-07-18 13:18:31 +0500

    fixed task "146. LRU Cache"

--- Algorithms/Tasks.0001.0500/0146.LRUCache/description.md
+++ Algorithms/Tasks.0001.0500/0146.LRUCache/description.md
@@ -32,6 +32,6 @@ lRUCache.get(4);    // return 4
 ##### Constraints:
 
 * ```1 <= capacity <= 3000```
-* ```0 <= key <= 3000```
-* ```0 <= value <= 10^4```
-* At most ```3 * 10^4``` calls will be made to ```get``` and ```put```.
+* ```0 <= key <= 10^4```
+* ```0 <= value <= 10^5```
+* At most ```3 * 10^5``` calls will be made to ```get``` and ```put```.
--- Algorithms/Tasks.0001.0500/0146.LRUCache/solution.cpp
+++ Algorithms/Tasks.0001.0500/0146.LRUCache/solution.cpp
@@ -1,104 +1,58 @@
-#include <array>
-#include <memory>
+#include <list>
+#include <unordered_map>
 
 #include "gtest/gtest.h"
 
 namespace
 {
 
-class LRUCacheItem
+class LRUCache
 {
 public:
-    LRUCacheItem(int key, int value) : key(key), value(value), prev(nullptr), next(nullptr)
+    LRUCache(int capacity) : _capacity(capacity)
     {
-    }
-
-    int key;
-    int value;
-    std::shared_ptr<LRUCacheItem> prev;
-    std::shared_ptr<LRUCacheItem> next;
-};
-
-class LRUCache {
-public:
-    LRUCache(int capacity) : _storage({}), _queueHead(nullptr), _queueTail(nullptr), _capacity(capacity), _size(0)
-    {
-        _storage.fill(nullptr);
     }
 
     int get(int key)
     {
-        if (_storage[key - MinKeyValue] == nullptr)
+        const auto iterator = _keys.find(key);
+        if (iterator == _keys.cend())
             return -1;
-        useItem(_storage[key - MinKeyValue]);
-        return _storage[key - MinKeyValue]->value;
+        const TEntry entry(*iterator->second);
+        _entries.erase(iterator->second);
+        _entries.push_front(entry);
+        iterator->second = _entries.cbegin();
+        return entry.second;
     }
 
     void put(int key, int value)
     {
-        if (_storage[key - MinKeyValue] == nullptr)
+        const auto iterator = _keys.find(key);
+        if (iterator == _keys.cend())
         {
-            std::shared_ptr<LRUCacheItem> item = std::make_shared<LRUCacheItem>(key, value);
-            _storage[key - MinKeyValue] = item;
-            // add to head
-            if (_size == 0)
-                _queueHead = _queueTail = item;
-            else
-            {
-                item->next = _queueHead;
-                _queueHead->prev = item;
-                _queueHead = item;
-            }
-            // remove tail if needed
-            if (_size == _capacity)
-            {
-                std::shared_ptr<LRUCacheItem> tail = _queueTail;
-                _queueTail = tail->prev;
-                _queueTail->next = nullptr;
-                _storage[tail->key - MinKeyValue] = nullptr;
-                tail.reset();
-            }
-            else
-                ++_size;
+            _entries.emplace_front(key, value);
+            _keys.emplace(key, _entries.cbegin());
         }
         else
         {
-            _storage[key - MinKeyValue]->value = value;
-            useItem(_storage[key - MinKeyValue]);
+            const TEntry entry(key, value);
+            _entries.erase(iterator->second);
+            _entries.push_front(entry);
+            iterator->second = _entries.cbegin();
+        }
+        if (_entries.size() > _capacity)
+        {
+            const TEntry lastEntry(_entries.back());
+            _entries.pop_back();
+            _keys.erase(lastEntry.first);
         }
     }
 
 private:
-    static constexpr int MinKeyValue = 0;
-    static constexpr int MaxKeyValue = 3000;
-
-    void useItem(std::shared_ptr<LRUCacheItem> const &item)
-    {
-        if (_size == 1)
-            return;
-        if (item->prev == nullptr)
-            return;
-        if (item->next == nullptr)
-        {
-            _queueTail = item->prev;
-            _queueTail->next = nullptr;
-        }
-        else
-        {
-            item->prev->next = item->next;
-            item->next->prev = item->prev;
-        }
-        _queueHead->prev = item;
-        item->prev = nullptr;
-        item->next = _queueHead;
-        _queueHead = item;
-    }
-
-    std::array<std::shared_ptr<LRUCacheItem>, MaxKeyValue - MinKeyValue + 1> _storage;
-    std::shared_ptr<LRUCacheItem> _queueHead;
-    std::shared_ptr<LRUCacheItem> _queueTail;
+    typedef std::pair<int, int> TEntry;
     size_t _capacity;
-    size_t _size;
+    std::unordered_map<int, std::list<TEntry>::const_iterator> _keys;
+    std::list<TEntry> _entries;
 };
 
 }
@@ -109,12 +63,16 @@ namespace LRUCacheTask
 TEST(LRUCacheTaskTests, Examples)
 {
     LRUCache lruCache(2);
-    lruCache.put(1, 1); // cache is {1=1}
-    lruCache.put(2, 2); // cache is {1=1, 2=2}
+    // cache is {1=1}
+    lruCache.put(1, 1);
+    // cache is {1=1, 2=2}
+    lruCache.put(2, 2);
     ASSERT_EQ(1, lruCache.get(1));
-    lruCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lruCache.put(3, 3);
     ASSERT_EQ(-1, lruCache.get(2));
-    lruCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lruCache.put(4, 4);
     ASSERT_EQ(-1, lruCache.get(1));
     ASSERT_EQ(3, lruCache.get(3));
     ASSERT_EQ(4, lruCache.get(4));
