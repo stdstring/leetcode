c788eb9bde280e496ec76a521305828e9e05dec6
stdstring <std_string@mail.ru>
2026-06-25 23:00:36 +0500

    added task "3737. Count Subarrays With Majority Element I"

---
+++ Algorithms/Tasks.3501.4000/3737.CountSubarraysWithMajorityElement1/solution.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+class Solution
+{
+public:
+    [[nodiscard]] int countMajoritySubarrays(std::vector<int> const &nums, int target) const
+    {
+        const size_t n = nums.size();
+        size_t result = 0;
+        size_t currentCount = n;
+        size_t currentPrefixSum = 0;
+        std::vector<size_t> prefixSums(2 * n + 1, 0);
+        prefixSums[currentCount] = 1;
+        for (int number : nums)
+        {
+            if (number == target)
+            {
+                currentPrefixSum += prefixSums[currentCount];
+                ++currentCount;
+                ++prefixSums[currentCount];
+            }
+            else
+            {
+                --currentCount;
+                currentPrefixSum -= prefixSums[currentCount];
+                ++prefixSums[currentCount];
+            }
+            result += currentPrefixSum;
+        }
+        return static_cast<int>(result);
+    }
+};
+
+}
+
+namespace CountSubarraysWithMajorityElement1Task
+{
+
+TEST(CountSubarraysWithMajorityElement1TaskTests, Examples)
+{
+    constexpr Solution solution;
+    ASSERT_EQ(5, solution.countMajoritySubarrays({1, 2, 2, 3}, 2));
+    ASSERT_EQ(10, solution.countMajoritySubarrays({1, 1, 1, 1}, 1));
+    ASSERT_EQ(0, solution.countMajoritySubarrays({1, 2, 3}, 4));
+}
+
+}
